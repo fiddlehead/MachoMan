a9abf25f8053f0ce3be19ac8a2eac3dccb51c774
Fiddlehead <rovingfiddlehead@gmail.com>
2014-01-22 16:48:46 -0600

    Adding Files

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+
+/*
+ * GET home page.
+ */
+
+exports.home = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+var jf = require('jsonfile');
+
+var file = 'data.json';
+
+entries= jf.readFileSync(file);
+
+
+exports.submit = function(req, res){
+  entries.push(req.body.Username);
+    jf.writeFileSync(file, entries);
+    res.render('submissions', 
+             { title: entries });
+};
+
+exports.visit = function(req, res){
+    res.render('submissions', 
+             { title: entries });
+};
+
+
+console.log(jf.readFileSync(file));
